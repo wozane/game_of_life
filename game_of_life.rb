eb69682ff7ecffe528d4f1825d07d3df9996c786
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-11-14 15:06:46 +0100

    Checks and counts neighbours to the north

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,25 +1,61 @@
+# all cells are dead at the beginning
 class Cell
-  attr_accessor :row, :column
+  attr_accessor :alive, :row, :column
 
   def initialize(row = 0, column = 0)
     @row = row
     @column = column
+    @alive = false
   end
 
-  def create_cell(row, column)
-    Cell.new(row, column)
+  def alive?
+    alive
   end
 
-  def neighbours
-    0
+  def dead?
+    !alive
   end
 end
 
+# it holds the place for the cells
 class Board
-  attr_accessor :rows, :columns
+  attr_accessor :rows, :columns, :cells_board
 
-  def initialize(rows = 0, columns = 0)
+  def initialize(rows = 3, columns = 3)
     @rows = rows
     @columns = columns
+    @cells_board = Array.new(rows) do |row|
+                    Array.new(columns) do |col|
+                      Cell.new(col, row) # this can be checked in irb when creating a Board.new
+                    end
+                  end
+  end
+
+  def neighbours(cell)
+    neighbours = []
+
+    #north
+    if cell.row > 0
+      new_cell = cells_board[cell.row - 1][cell.column]
+      neighbours << new_cell if new_cell.alive?
+    end
+    neighbours
+  end
+end
+
+# here the board and cells meet
+class Round
+  attr_accessor :board, :cells
+
+  def initialize(board = Board, cells = [])
+    @board = board
+    @cells = cells
+
+    cells.each do |cell|
+      board.cells_board[cell[0]][cell[1]].alive = true
+    end
+  end
+
+  def tick!
   end
 end
--- game_of_life_test.rb
+++ game_of_life_test.rb
@@ -2,34 +2,82 @@ require 'rspec'
 require_relative 'game_of_life'
 
 describe 'Game of life' do
+  let!(:board) { Board.new }
+  # let!(:cell) { Cell.new(1, 1) }
+
   context 'Board' do
     subject { Board.new }
 
     it 'initializes' do
-      subject.is_a?(Board).equal? true
+      expect(subject).to be_truthy
     end
 
     it 'responds to rows() and columns()' do
       expect(subject).to respond_to(:rows)
       expect(subject).to respond_to(:columns)
+      expect(subject).to respond_to(:cells_board)
+      expect(subject).to respond_to(:neighbours)
+    end
+
+    it 'creates itself i.e. board :)' do
+      expect(subject.cells_board).to be_instance_of(Array)
+
+      subject.cells_board.each do |row|
+        expect(row).to be_instance_of(Array)
+      end
+    end
+
+    it 'checks count neighbours' do
+      expect(subject.cells_board[0][1]).to have_attributes(alive: false)
+      new_one = subject.cells_board[0][1]
+      new_one.alive = true
+      expect(new_one).to have_attributes(alive: true)
+      expect(subject.neighbours(subject.cells_board[1][1]).count).to eq(1)
     end
   end
 
   context 'Cell' do
     subject { Cell.new }
 
+    it 'initializes properly' do
+      expect(subject).to be_truthy
+      expect(subject).to have_attributes(alive: false)
+    end
+
+    it 'responds to methods' do
+      expect(subject).to respond_to(:alive)
+      expect(subject).to respond_to(:row)
+      expect(subject).to respond_to(:column)
+    end
+  end
+
+  context 'Round' do
+    subject { Round.new }
+
+    it 'responds to board and cells' do
+      expect(subject).to respond_to(:board)
+      expect(subject).to respond_to(:cells)
+    end
+
     it 'initializes' do
-      subject.is_a?(Cell).equal? true
+      expect(subject.board).to match(Board)
+      expect(subject.cells).to match([])
     end
 
-    it 'is created' do
-      cell = subject.create_cell(1, 2)
-      expect(cell.row).to be(1)
-      expect(cell.column).to be(2)
+    it 'has board with alive cells' do
+      first_round = Round.new(board, [[1, 0], [0, 2]])
+      expect(board.cells_board[1][0]).to have_attributes(alive: true)
+      expect(board.cells_board[0][2]).to have_attributes(alive: true)
     end
+  end
 
-    it '#1: cell with fewer than 2 neighbours dies' do
-      expect(subject.neighbours).to equal(0)
+  xcontext 'Rule #1' do
+    let!(:round) { Round.new }
+
+    it 'Cell dies if has one neighbour' do
+      first_round = Round.new(board, [[1, 0], [0, 2]])
+      first_round.tick!
+      expect(board.cells_board[1][0]).to have_attributes(alive: false)
     end
   end
 end
